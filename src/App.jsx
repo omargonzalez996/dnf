f76bd3e9770ef87c0ed79d9d6d6ed8cf35df7754
Omar Gonzalez <108283912+omargonzalez996@users.noreply.github.com>
2024-11-22 11:27:55 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <>
+      <div className="bg-slate-400 p-10">
+        <Button variant="contained">PRESS ME!</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
