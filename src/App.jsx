0470981f2e80888ded7d8e08eac7f7adb36465a5
Omar Gonzalez <108283912+omargonzalez996@users.noreply.github.com>
2024-11-29 16:03:17 -0600

    fixed navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
-import Button from "@mui/material/Button";
-
+import NavMenu from "./components/NavMenu";
+import SpaIcon from "@mui/icons-material/Spa";
 function App() {
   return (
     <>
-      <div className="bg-slate-400 p-10">
-        <Button variant="contained">PRESS ME!</Button>
+      <div className="p-2 shadow-md flex flex-row-reverse">
+        {/* Navbar */}
+        <div className="w-1/2 flex items-center justify-end">
+          <NavMenu />
+        </div>
+        <div className="w-1/2 flex items-center justify-start px-5 font-light justify-between">
+          <SpaIcon />
+          <p className="text-sm">D&F LANDSCAPING</p>
+        </div>
       </div>
     </>
   );
