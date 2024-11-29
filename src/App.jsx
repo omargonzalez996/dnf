5131fffcd9c9385bf6b117c6b4d696112d6b3853
Omar Gonzalez <108283912+omargonzalez996@users.noreply.github.com>
2024-11-29 16:03:48 -0600

    aaasdasdasd

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
         <div className="w-1/2 flex items-center justify-end">
           <NavMenu />
         </div>
-        <div className="w-1/2 flex items-center justify-start px-5 font-light justify-between">
+        <div className="w-1/2 flex items-center px-5 font-light justify-between">
           <SpaIcon />
           <p className="text-sm">D&F LANDSCAPING</p>
         </div>
